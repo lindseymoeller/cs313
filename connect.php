29b46f50885a1c835b56a40ea5106f455d78d321
Lindsey Moeller <Lindsey Moeller>
2016-01-26 15:23:27 -0700

    added connect.php test

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbHost = '';
+$dbPort = '';
+$dbUser = '';
+$dbPassword = '';
+$dbName = 'php';
+
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
+	
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+?>
