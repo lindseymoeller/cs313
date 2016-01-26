b12adaaebd4e8a516495159c4ae7cc08d141e2a2
Lindsey Moeller <Lindsey Moeller>
2016-01-26 15:37:08 -0700

    datadase connection

--- connect.php
+++ connect.php
@@ -1,18 +1,23 @@
 <?php
 
-$dbHost = '';
-$dbPort = '';
-$dbUser = '';
-$dbPassword = '';
-$dbName = 'php';
+function dbConnect() {
 
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$dbHost = '';
+	$dbPort = '';
+	$dbUser = '';
+	$dbPassword = '';
+	$dbName = 'php';
 
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');	
+	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+
+	//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
 	
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser,				$dbPassword);
+
+		return $db;
+}
 
 ?>
