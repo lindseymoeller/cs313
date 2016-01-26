9f0b19958d0457357272c9bad56a7682a4d82542
Lindsey Moeller <Lindsey Moeller>
2016-01-26 16:56:38 -0700

    updated connect

--- connect.php
+++ connect.php
@@ -7,13 +7,24 @@ function dbConnect() {
 	$dbUser = '';
 	$dbPassword = '';
 	$dbName = 'php';
-
-	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');	
-	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-
-	//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
+	
+	$onOpenShift = $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	
+	if($onOpenShift === null || $onOpenShift == '') {
+		// in localhost environment
+		$dbHost = '127.0.0.1';
+		$dbPort = '8889';
+		$dbUser = 'root';
+		$dbPassword = 'root';
+	} else {
+		//in openshift environment
+		$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');	
+		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+		$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+		$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	}
+	
+	echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
 	
 	$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser,				$dbPassword);
 
